f6b59a2d84cf2300cbf7cffdbd413a72c6a096a3
arvind.kumar <arvind.kumar@10.1.1.107>
2015-03-06 14:19:32 -0700

    replace database password

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,7 +3,7 @@ dataSource {
     jmxExport = true
 	driverClassName = "com.mysql.jdbc.Driver"
     username = "root"
-    password = "plvr00t"
+    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
